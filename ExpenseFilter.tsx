d652c043e58c7ee386b4c2f0f74983b4127357e3
hb351 <boustta.elh@gmail.com>
2024-05-16 17:12:21 +0100

    complete and validate the expense form

---
+++ ExpenseFilter.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import categories from "./categories";
+
+interface Props {
+  onSelectCategory: (category: string) => void;
+}
+
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
+  return (
+    <div className="mb-3">
+      <select
+        className="form-select"
+        onChange={(event) => onSelectCategory(event.target.value)}
+      >
+        <option value="">All categories</option>
+        {categories.map((c) => (
+          <option key={c} value={c}>
+            {c}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default ExpenseFilter;
